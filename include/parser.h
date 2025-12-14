32435bca3c6fea2acfa123bd7d489cd9328ad5b4
Alex Curran <haletas88@gmail.com>
2025-12-14 21:47:49 +1300

    Add parser class

---
+++ include/parser.h
@@ -0,0 +1,19 @@
+//
+// Created by halet on 12/14/2025.
+//
+
+#ifndef PARSER_H
+#define PARSER_H
+
+typedef const char* data;
+
+class Parser {
+private:
+    data data_;
+public:
+    explicit Parser(data d);
+};
+
+
+
+#endif //PARSER_H
---
+++ src/parser.cpp
@@ -0,0 +1,7 @@
+//
+// Created by halet on 12/14/2025.
+//
+
+#include "../include/parser.h"
+
+Parser::Parser(const data d) : data_(d) {};
