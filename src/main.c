c34564a85330d9f2bed7662aed9af9efa572282c
Alex Curran <haletas88@gmail.com>
2026-01-05 12:20:26 +1300

    Actually support both linux and windows

--- src/main.c
+++ src/main.c
@@ -38,7 +38,7 @@ int main() {
 
     CleanUpData("C:/Users/halet/Dev/wikianalyserTest.xml", unwanted);
 
-    ParseArticles("C:/Users/halet/Dev/wikianalyserTest.xml", &articles, &article, &articleCount));
+    ParseArticles("C:/Users/halet/Dev/wikianalyserTest.xml", &articles, &article, &articleCount);
 
     #endif
 
